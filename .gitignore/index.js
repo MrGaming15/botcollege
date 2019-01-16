021634ec14f2957ff581b53806a52335c0bedc97
MrGaming15 <44035101+MrGaming15@users.noreply.github.com>
2019-01-16 17:11:17 +0100

    Add files via upload

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,6 +16,10 @@ bot.on('guildMemberAdd', function(member){
 })
  bot.on('message', function(message){
  	if (message.content.startsWith('!')) {
+ 	 if (!message.guild){
+ 	 	message.reply('Je ne fonctionne que sur serveur.')
+ 	 	return
+ 	 }
    	 if(message.content.startsWith(prefix+"aide")) {
     		let args = message.content.split(' ')
     		args.shift()
@@ -24,19 +28,24 @@ bot.on('guildMemberAdd', function(member){
 
  		}
  	 if (message.content === prefix+'ping'){
- 			let ping = message.author.client.ping
- 			message.author.createDM().then(function (channel){
- 				return channel.send('Votre ping est de : '+ping+'ms')
- 			})
+ 		let ping = message.author.client.ping
+ 		return message.channel.send('Votre ping est de '+ping+'ms')
+
 
  		}
  	 if (message.content === prefix+'help'){
- 	 	message.channel.send("Voici la liste des commandes disponible : \n - !aide <message> Sert à notifier tout les membres présents avec votre message (A utiliser en cas de véritable besoin d'aide)\n - !ping Donne votre ping en milliseconde")
-
+ 	 	const embed = new Discord.RichEmbed()
+ 	 	  .setAuthor('Collège Louis Pasteur (15)',message.client.user.avatarURL)
+ 	 	  .setTitle('Les commandes du serveur')
+ 	 	  .setColor('#FFFFFF')
+ 	 	  .setDescription("- !aide <message> Sert à notifier tout les membres présents avec votre message (A utiliser en cas de véritable besoin d'aide)\n - !ping Donne votre ping en milliseconde");
+ 	 	message.channel.send(embed)
+ 	 	
+ 	 	return
  	 }
- 	 else{
- 	 	message.reply("Cette commande n'existe pas.\n Voici la liste des commandes disponible : \n - !aide <message> Sert à notifier tout les memprésents avec votre message (A utiliser en cas de véritable besoin d'aide)\n - !ping Donne votre ping en milliseconde")
- 	 }
+ 	if (!message.content === prefix+'help' || !message.content.startsWith(prefix+'aide') || !message.content === prefix+'ping' ){
+ 	 	message.reply("Je n'ai pas compris :thinking:. Tapez !help pour plus d'information. Si la commande que vous venez de taper est inscrite dans le !help mais ne fonctionne pas merci de contacter un Modérateur.")
  	}
+ }
  })
- bot.login('NTMyNDM2MzQ5MTM1ODgwMTkz.DyC3YQ.D21yzTObVuCZE7haiz88k3gBd9Y')
+ bot.login('NTMyNDM2MzQ5MTM1ODgwMTkz.DyDiPg.8nq45s7Zt6qDYp_7hKfL1Kb9pAI') 	
