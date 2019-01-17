87bb4fefc28156215988be01c275e66a5a001644
MrGaming15 <44035101+MrGaming15@users.noreply.github.com>
2019-01-17 07:27:48 +0100

    Add files via upload

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 let prefix = '!'
 bot.on('ready', function (){
-	bot.user.setActivity('le cours de Maths', { type: 'LISTENING' })
+	bot.user.setActivity('!help', { type: 'LISTENING' })
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
   .catch(console.error);
 	//bot.user.setAvatar('./avatar.png')
@@ -15,11 +15,8 @@ bot.on('guildMemberAdd', function(member){
 	})
 })
  bot.on('message', function(message){
- 	if (message.content.startsWith('!')) {
- 	 if (!message.guild){
- 	 	message.reply('Je ne fonctionne que sur serveur.')
- 	 	return
- 	 }
+ 	if (message.content.startsWith(prefix)) {
+ 	 if (!message.guild)return;
    	 if(message.content.startsWith(prefix+"aide")) {
     		let args = message.content.split(' ')
     		args.shift()
@@ -30,7 +27,7 @@ bot.on('guildMemberAdd', function(member){
  	 if (message.content === prefix+'ping'){
  		let ping = message.author.client.ping
  		return message.channel.send('Votre ping est de '+ping+'ms')
-
+ 		
 
  		}
  	 if (message.content === prefix+'help'){
@@ -38,14 +35,37 @@ bot.on('guildMemberAdd', function(member){
  	 	  .setAuthor('Collège Louis Pasteur (15)',message.client.user.avatarURL)
  	 	  .setTitle('Les commandes du serveur')
  	 	  .setColor('#FFFFFF')
- 	 	  .setDescription("- !aide <message> Sert à notifier tout les membres présents avec votre message (A utiliser en cas de véritable besoin d'aide)\n - !ping Donne votre ping en milliseconde");
- 	 	message.channel.send(embed)
+ 	 	  .setDescription("- !aide <message> Sert à notifier tout les membres présents avec votre message (A utiliser en cas de véritable besoin d'aide)\n - !ping Donne votre ping en milliseconde\n - !ressource Permet d'obtenir le lien vers les ressources.");
+ 	 	return message.channel.send(embed)
  	 	
- 	 	return
  	 }
- 	if (!message.content === prefix+'help' || !message.content.startsWith(prefix+'aide') || !message.content === prefix+'ping' ){
- 	 	message.reply("Je n'ai pas compris :thinking:. Tapez !help pour plus d'information. Si la commande que vous venez de taper est inscrite dans le !help mais ne fonctionne pas merci de contacter un Modérateur.")
- 	}
+ 	 if(message.content === prefix+'ressource'){
+ 	 	return message.channel.send('Voici le lien vers les ressources : https://bit.ly/2Me3Mn6')
+ 	 
+ 	 }
+ 	 if(message.content === prefix+'infos'){
+ 	 	let size = message.client.users.size
+ 	 	size = size-1
+ 	 	const embed = new Discord.RichEmbed()
+ 	 	  .setAuthor('Collège Louis Pasteur (15)',message.client.user.avatarURL)
+ 	 	  .addField('Langage de programmation :', 'NodeJS')
+ 	 	  .addField('Bibliothèque :', 'discord.js')
+ 	 	  .addField('Version du bot :', '1.0.0')
+ 	 	  .addField('Nombre de personne sur le serveur :', size)
+ 	 	  .addField('Développeur :', "Mr_gaming_15#6367")
+ 	 	  .setColor('#0000ff')
+ 	 	message.channel.send(embed)
+ 	 }
+ 	 else{
+ 	 	message.channel.send("Je n'ai pas compris :thinking:. Tapez !help pour plus d'information.")
+ 	 }
+ 	 
+
+ 	 
+ 	 
+ 	 	 
+ 	 	 
+ 	
  }
  })
- bot.login(process.env.TOKEN) 	
+ bot.login('NTMyNDM2MzQ5MTM1ODgwMTkz.DyGsEg.au0GlxwG4v1RQAt09lt5o2x7uo4') 	
